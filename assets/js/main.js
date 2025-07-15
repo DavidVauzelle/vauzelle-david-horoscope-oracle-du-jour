3814ac3d7c3e08af3207d9253b819b4732ba53ba
David Vauzelle <david.vauzelle@CNED.ORG>
2025-07-15 15:42:47 +0200

    - Implémentation de fetch() avec méthode POST et clé API
    - Affichage d'une barre de chargement pendant la requête
    
    - Affichage d’un message d’erreur utilisateur en cas d’échec API
    
    - Nettoyage de l’ancien résultat avant chaque nouveau tirage
    
    - Test temporaire : affichage brut du JSON dans la page
    
    - Modifications légères du html, de nom d'id et de constantes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,9 +11,7 @@ const baseUrl = "https://oracles-api.sidathsoeun.fr/oracle_api.php";
 // Préparer les options fetch de base
 const fetchOptions = {
     method: 'POST',
-    headers: {
-    'Content-Type': 'application/json'
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ api_key: apiKey })
 };
 
@@ -21,11 +19,46 @@ const fetchOptions = {
 const sectionTirage = document.querySelector('.section-tirage');
 const btnTirage = document.getElementById('btn-tirage');
 // Seront créés dynamiquement (loader, erreur, résultats)
-const loader = document.getElementById('loader');
-const erreurMessage = document.getElementById('erreur-message');
+const chargement = document.getElementById('chargement');
+const erreurMessage = "Une erreur est survenue. Veuillez réessayer.";
 const resultatApi = document.getElementById('resultat-api');
 
 // On capture le clic sur le bouton de tirage
 btnTirage.addEventListener('click', function() {
     lancerTirage();
 });
+
+// Fonction pour récupérer et afficher l'horoscope des 12 signes
+async function lancerTirage() {
+    // Vider les résultats précédents
+    resultatApi.innerHTML = "";
+
+    // Affiche une barre de chargement à chaque tirage
+    chargement.hidden = false;
+    chargement.innerHTML = "<p>Chargement en cours…</p>"; // barre de chargement à styliser
+    // chargement.innerHTML = "<div class='bar-chargement'></div>"; // Pour afficher la barre de chargement une fois stylisée
+
+    try {
+        // Requête POST vers l'API avec clé et signe
+       const response = await fetch(baseUrl, fetchOptions);
+
+        // Affiche un message d'erreur si la réponse est incorrecte
+        if (!response.ok) throw new Error(erreurMessage);
+
+        // Conversion de la réponse en JSON
+        const data = await response.json();
+
+        // Affichage temporaire des données récupérées (formaté JSON)
+        resultatApi.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+
+    } catch (error) {
+        // Affiche un message d'erreur si la requête échoue
+        resultatApi.innerHTML = `<p style="color:red;">Erreur : ${error.message}</p>`;
+    }
+
+    finally {
+        // Cache le barre de chargement après la requête
+        chargement.innerHTML = "";
+        chargement.hidden = true;
+    }
+}
